1951f9165a0a6f77771578164e046471d42ae977
Dmitry Davydov <dvdvdmt@gmail.com>
2015-01-10 21:13:13 +0500

    Added a couple of new widgets

--- src/client/Hello.java
+++ src/client/Hello.java
@@ -1,49 +1,44 @@
 package client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Command;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
+import com.google.gwt.user.datepicker.client.DatePicker;
 
 /**
  * Created by dmitry on 07.01.15.
  */
 public class Hello implements EntryPoint {
-    Button button1= new Button("Button 1");
-    Form form1= new Form();
-    Button face = new Button("('.')");
-    Button changeFace = new Button("Click to change face");
-    int i =0;
+    DatePicker datePicker= new DatePicker();
 
+    MenuBar support;
+    MenuBar liveteam;
+    MenuBar paymentSystems;
+    MenuBar mainMenue;
     public void onModuleLoad() {
-        button1.addClickHandler(new ClickHandler() {
+        RootPanel.get("calendar").add(datePicker);
+
+        Command cmd = new Command(){
             @Override
-            public void onClick(ClickEvent event) {
-                Window.alert("Hello I am a "+button1.getText());
+            public void execute() {
+                Window.alert("I'm "+ support.getTitle());
             }
-        });
-        form1.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                Window.alert("Hello I am a "+form1.getText());
-            }
-        });
-        changeFace.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                String newFace="(-";
-                for (int j = i; j > 0; j--) {
-                    newFace += "_";
-                }
-                newFace+="-)";
-                i++;
-                face.setText(newFace);
-            }
-        });
-        RootPanel.get("btn1").add(button1);
-        RootPanel.get().add(form1);
-        RootPanel.get().add(changeFace);
-        RootPanel.get().add(face);
+        };
+        support = new MenuBar(true);
+        liveteam = new MenuBar(true);
+        paymentSystems = new MenuBar(true);
+        mainMenue = new MenuBar(true);
+        mainMenue.setFocusOnHoverEnabled(true);
+        support.addItem("o.kondratova",cmd);
+        support.addItem("e.musinova",cmd);
+        liveteam.addItem("d.davydov",cmd);
+        liveteam.addItem("m.sharishev",cmd);
+        paymentSystems.addItem("i.oborin",cmd);
+        paymentSystems.addItem("o.anferov",cmd);
+        mainMenue.addItem("Support",support);
+        mainMenue.addItem("Liveteam",liveteam);
+        mainMenue.addItem("Payment Systems Integration",paymentSystems);
+        RootPanel.get().add(mainMenue);
     }
 }
